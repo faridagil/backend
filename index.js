2f36a5e4540598cb9ba2e4eef9c92bc53175b77a
faridagil <78220195+faridagil@users.noreply.github.com>
2021-05-30 01:42:36 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+const connection = mysql.createConnection({
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'agil'
+})
+
+connection.connect();
+
+app.get('/', (req,res) => {
+    res.send(`<html>
+        <body>
+            <form action="/todo" method="post">
+                <input name="deskripsi" />
+                <button>Add</button>
+            </form>
+        </body>
+    </html>`)
+})
+
+app.post('/todo', (req,res) => {
+    var params = req.body.deskripsi
+        connection.query("insert into bebas values(null, ?)", params, (err, rows, fields) =>{
+            if(err) throw err
+                res.end()
+        })
+})
+
+app.get('/todo', (req, res, fields) => {
+        connection.query('SELECT deskripsi from bebas', (err, rows, fields) =>{
+            if(err) throw err
+            rows = JSON.stringify(rows);
+            res.send(rows)
+        })
+})
+
+app.listen(3000, function(err){
+    console.log("Server started");
+})
